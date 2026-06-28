72fe0a366e7276da732a517b34e5fde69af33042
ryanchan718-ops <ryanchan718@gmail.com>
2026-06-28 12:10:48 +0100

    A shopping list project
    I used variables, print, input and operators in python.

---
+++ hw2.py
@@ -0,0 +1,5 @@
+apples=int(input("How many apples do you want to buy?"))
+oranges=int(input("How many oranges do you want to buy?"))
+bananas=int(input("How many bananas do you want to buy?"))
+
+print(f"Your total bill is {apples*3 + oranges*4 + bananas*2} pounds.")
